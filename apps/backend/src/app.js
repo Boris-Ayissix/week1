d50b0c1d3fe5495cc18e130de79bbc9d80f15480
Boris-Ayissix <borisayissix@gmail.com>
2026-04-18 17:39:01 +0100

    fix: correct API base URL

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -12,13 +12,29 @@ const app = express();
  * CORS FIX (IMPORTANT)
  */
 // Middleware
+
+const allowedOrigins = [
+  "http://localhost:5173",//local dev
+  "https://branding-website-five.vercel.app"//production frontend
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://https://branding-website-five.vercel.app/"
-  ]
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "x-admin-password"],
+
 }));
 app.use(express.json());
+
 
 /**
  * HEALTH CHECK
